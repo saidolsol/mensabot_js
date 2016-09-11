d1739b3d6e1e740b8fc4dea889f8d847033d70d7
Luca Dahle <admin@lucas-air.speedport_w_723v_1_40_000>
2016-09-11 18:29:24 +0200

    edited how

--- openingh.js
+++ openingh.js
@@ -6,7 +6,7 @@ var openingh ={
             "beersnmore":'*Öffnungszeiten Beers\'n\'More:* \nMo-Do: 16:00 bis 21:30 Uhr\nFr: 16:00 bis 22:00 Uhr\nSa: 12:00 bis 22:00 Uhr\nSonntags geschlossen',
             "jimmy" : "*Öffnungszeiten Jimmy\'s Pizza:*\nMo-Sa: 17:00 bis 23:30 Uhr\nSonntags geschlossen",
             "cc" : "*Öffnungszeiten Curry Corner:*\nMo-Fr: 11:15 bis 14:00 Uhr",
-            "how" : "Ich laufe auf [Node.js](nodejs.org) mit [dieser](https://github.com/yagop/node-telegram-bot-api) library. Die Menüs lade ich mir jeden tag als JSON bei der ETH runter. Alles weitere gibt\'s auf [GitHub](https://github.com/saidolsol/mensabot_js)"
+            "how" : "Ich laufe auf Node.js auf einem Server vom SOSETH. Die Menüs lade ich mir jeden tag als JSON bei der ETH runter. Alles weitere gibt\'s auf [GitHub](https://github.com/saidolsol/mensabot_js)"
         };
 
 module.exports = openingh;
