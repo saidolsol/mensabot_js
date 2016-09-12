b808d9aefe4807ef44fd3959e4c1386c61434812
Luca Dahle <admin@lucas-air.speedport_w_723v_1_40_000>
2016-09-12 09:52:08 +0200

    added case no menu

--- botManager.js
+++ botManager.js
@@ -89,16 +89,20 @@ function processOnText(msg, match) {
                 resp = "Heute haben leider alle Mensen geschlossen, sorry!";
             //If weekday, wanted information is formatted as follows:
             }else{
-                resp = "*" + command + "*\n_Essen von " + mensas[command].hours.mealtime[t]["from"] +" bis " +mensas[command].hours.mealtime[t]["to"] + " Uhr_\n\n";
-                for (var meal in mensas[command]["meals"]){
-                    var description = "";
-                    for (i in mensas[command]["meals"][meal]["description"]){
-                        description += mensas[command]["meals"][meal]["description"][i]+ " " ;
-                        if (i === "0"){
-                            description += "\n"; 
+                if (mensas[command] != undefined){
+                    resp = "*" + command + "*\n_Essen von " + mensas[command].hours.mealtime[t]["from"] +" bis " +mensas[command].hours.mealtime[t]["to"] + " Uhr_\n\n";
+                    for (var meal in mensas[command]["meals"]){
+                        var description = "";
+                        for (i in mensas[command]["meals"][meal]["description"]){
+                            description += mensas[command]["meals"][meal]["description"][i]+ " " ;
+                            if (i === "0"){
+                                description += "\n"; 
+                            }
                         }
+                        resp += "*" +mensas[command]["meals"][meal]["label"] + " (" + mensas[command]["meals"][meal]["prices"]["student"]+ "/" +mensas[command]["meals"][meal]["prices"]["staff"] +  "/" + mensas[command]["meals"][meal]["prices"]["extern"]+"):*\n" + description + "\n";
                     }
-                    resp += "*" +mensas[command]["meals"][meal]["label"] + " (" + mensas[command]["meals"][meal]["prices"]["student"]+ "/" +mensas[command]["meals"][meal]["prices"]["staff"] +  "/" + mensas[command]["meals"][meal]["prices"]["extern"]+"):*\n" + description + "\n";
+                }else{
+                    resp = "Tut mir Leid, für diese Mensa ist momentan leider kein Menü verfügbar :(";
                 }
             }
         }
