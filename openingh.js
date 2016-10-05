e68b27a021c9faf8255de0cd4a01d9a781165190
Luca Dahle <l.dahle@icloud.com>
2016-10-05 15:16:12 +0200

    Update openingh.js

--- openingh.js
+++ openingh.js
@@ -1,7 +1,7 @@
 var help_msg = '*Verfügbare Mensen*:\nMensa Polyterasse: /poly \nFood Trailer: /trailer \nCLAusiusbar: /asia\nFusionmensa: /fusion \nFoodLAB: /foodlab\nWoka: /woka\nTannenbar: /tannenbar\nFood Market: /grill, /pizzapasta, /green\nDozentenfoyer: /dozentenfoyer\n\nAbendmenüs gibt\'s ab 14:00 Uhr.\n\n*Verfügbare Öffnungszeiten:*\n_Mit Vorsicht zu genießen!_\nKlara\'s Kitchen: /klaras \nBeers\'n\'More: /beersnmore\nHot Pasta: /hotpasta\nJimmy\'s Pizza: /jimmy\nCurry Corner: /cc  \n\n*Fragen oder Anregungen:*\n/feedback <Deine Nachricht>\n\n*Willst Du mithelfen?*\nZum Beispiel die UZH-Mensen hinzufügen? Schau doch auf [GitHub](https://github.com/saidolsol/mensabot_js) vorbei :)';
 
 var openingh ={
-            "klaras":'*Öffnungszeiten Klara\'s Kitchen:* \nMo-Fr: 08:00 bis 18:30 Uhr\nSamstags und Sonntags geschlossen',
+            "klaras":'*Öffnungszeiten Klara\'s Kitchen:* \nMo-Fr: 07:30 bis 16:30 Uhr\nSamstags und Sonntags geschlossen',
             "hotpasta":'*Öffnungszeiten Hot Pasta:* \nMo-Fr: 08:00 bis 24:00 Uhr\nSa: 11:00 bis 17:00 Uhr\nSonntags geschlossen',
             "beersnmore":'*Öffnungszeiten Beers\'n\'More:* \nMo-Do: 16:00 bis 21:30 Uhr\nFr: 16:00 bis 22:00 Uhr\nSa: 12:00 bis 22:00 Uhr\nSonntags geschlossen',
             "jimmy" : "*Öffnungszeiten Jimmy\'s Pizza:*\nMo-Sa: 17:00 bis 23:30 Uhr\nSonntags geschlossen",
