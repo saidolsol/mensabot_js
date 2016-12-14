cd71ad32d62b4908a56d4b02ca3a7abef56722f6
degygii <lukas.gygi@gmail.com>
2016-12-14 13:55:26 +0100

    added commands for new mensas and opening hours

--- openingh.js
+++ openingh.js
@@ -1,4 +1,33 @@
-var help_msg = '*Verfügbare Mensen*:\nMensa Polyterasse: /poly \nFood Trailer: /trailer \nCLAusiusbar: /asia\nFusionmensa: /fusion \nFoodLAB: /foodlab\nWoka: /woka\nTannenbar: /tannenbar\nFood Market: /grill, /pizzapasta, /green\nDozentenfoyer: /dozentenfoyer\nUZH untere Mensa a: /uniunten\nUZH obere Mensa b: /unioben\nUZH Lichthof: /lichthof\nUZH Irchel: /irchel\nUZH Tierspital: /tierspital\nUZH Zahnmedizin: /zahnarzt\nUZH Plattenstrasse: /platte\nUZH Rämi 59 (vegan): /raemi\n\nAbendmenüs gibt\'s ab 14:00 Uhr.\n\n*Verfügbare Öffnungszeiten:*\n_Mit Vorsicht zu genießen!_\nKlara\'s Kitchen: /klaras \nBeers\'n\'More: /beersnmore\nHot Pasta: /hotpasta\nJimmy\'s Pizza: /jimmy\nCurry Corner: /cc  \n\n*Fragen oder Anregungen:*\n/feedback <Deine Nachricht>\n\n*Willst Du mithelfen?*\nSchau doch auf [GitHub](https://github.com/saidolsol/mensabot_js) vorbei :)';
+var help_msg = '*Verfügbare Mensen*:\n\
+    Mensa Polyterasse: /poly \n\
+    Food Trailer: /trailer \n\
+    CLAusiusbar: /asia\n\
+    Fusionmensa: /fusion \n\
+    FoodLAB: /foodlab\n\
+    Woka: /woka\n\
+    Polysnack: /polysnack\n\
+    G-ESSbar: /gessbar\n\
+    Tannenbar: /tannenbar\n\
+    Food Market: /grill, /pizzapasta, /green\n\
+    Dozentenfoyer: /dozentenfoyer\n\
+    Alumni quattro Lounge: /alumni\n\
+    UZH untere Mensa a: /uniunten\n\
+    UZH obere Mensa b: /unioben\n\
+    UZH Lichthof: /lichthof\n\
+    UZH Irchel: /irchel\n\
+    UZH Tierspital: /tierspital\n\
+    UZH Zahnmedizin: /zahnarzt\n\
+    UZH Plattenstrasse: /platte\n\
+    UZH Rämi 59 (vegan): /raemi\n\n\
+    Abendmenüs gibt\'s ab 14:00 Uhr.\n\n*Verfügbare Öffnungszeiten:*\n_Mit Vorsicht zu genießen!_\n\
+    Klara\'s Kitchen: /klaras \n\
+    Unipoint Döner: /unipoint\n\
+    coop Haldenbach: /coop\n\
+    Beers\'n\'More: /beersnmore\n\
+    Hot Pasta: /hotpasta\n\
+    Jimmy\'s Pizza: /jimmy\n\
+    Curry Corner: /cc  \
+    \n\n*Fragen oder Anregungen:*\n/feedback <Deine Nachricht>\n\n*Willst Du mithelfen?*\nSchau doch auf [GitHub](https://github.com/saidolsol/mensabot_js) vorbei :)';
 
 var openingh ={
             "klaras":'*Öffnungszeiten Klara\'s Kitchen:* \nMo-Fr: 07:30 bis 16:30 Uhr\nSamstags und Sonntags geschlossen',
@@ -6,6 +35,8 @@ var openingh ={
             "beersnmore":'*Öffnungszeiten Beers\'n\'More:* \nMo-Do: 16:00 bis 21:30 Uhr\nFr: 16:00 bis 22:00 Uhr\nSa: 12:00 bis 22:00 Uhr\nSonntags geschlossen',
             "jimmy" : "*Öffnungszeiten Jimmy\'s Pizza:*\nMo-Sa: 17:00 bis 23:30 Uhr\nSonntags geschlossen",
             "cc" : "*Öffnungszeiten Curry Corner:*\nMo-Fr: 11:15 bis 14:00 Uhr",
+            "coop": "*Öffnungszeiten coop Haldenbach:*\nMo-Sa: 07:30 bis 21:00 Uhr\nSonntags geschlossen",
+            "unipoint": "*Öffnungszeiten Döner Unipoint:*\nMo-Fr: 08:30 bis 23:30 Uhr\nSa-So: 10:30 bis 22:00 Uhr",
             "how" : "Ich laufe auf Node.js auf einem Server vom SOSETH. Die Menüs lade ich mir jeden tag als JSON bei der ETH runter. Alles weitere gibt\'s auf [GitHub](https://github.com/saidolsol/mensabot_js).",
             "help" : help_msg,
             "start" : help_msg
